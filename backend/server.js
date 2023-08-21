19d3c5aff8b1eef79206cc27ec514eeca7aa3bdc
Barnakrum <krzysiu03@tlen.pl>
2023-08-21 14:33:40 +0200

    Disabled cors for development

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 port = process.env.PORT;
 
 const app = express();
+
+if (process.env.ENV === "DEVELOPMENT") {
+    app.use(cors({ origin: "*" }));
+} else {
+    app.use(cors({ origin: process.env.FRONTEND_ORIGIN }));
+}
 
 app.get("/", (req, res) => {
     res.send("hello");
