7026870023904fbacbda2ceff750a5bdd360aebc
Barnakrum <krzysiu03@tlen.pl>
2023-08-22 14:09:55 +0200

    Basic validation and httpOnly cookie in register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userLogin = (req, res) => {
     res.send("login route");
@@ -6,8 +8,28 @@ const userLogin = (req, res) => {
 
 const userRegister = async (req, res) => {
     try {
-        const user = await User.create({ ...req.body });
-        res.status(200).send(user);
+        const { username, email, password } = req.body;
+
+        if (!(username && email && password)) {
+            return res.status(400).send("Please fill all fields");
+        }
+
+        const collidingEmail = await User.findOne({ email });
+        const collidingUsername = await User.findOne({ username });
+        if (!!collidingEmail) {
+            return res.status(400).send("User with that email already exists");
+        }
+        if (!!collidingUsername) {
+            return res.status(400).send("User with that username already exists");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({ username, email, password: hashedPassword });
+
+        const token = jwt.sign({ user_id: user._id, email }, process.env.TOKEN_KEY, { expiresIn: "1d" });
+
+        res.status(201).cookie("session", token, { secure: true, httpOnly: true, sameSite: "strict" }).send({ username });
     } catch (error) {
         res.status(400).send(error.message);
     }
