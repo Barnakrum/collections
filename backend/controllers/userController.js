b3581cb81fec9a32e4312e3563cd0a460d27302d
Barnakrum <krzysiu03@tlen.pl>
2023-08-22 13:15:26 +0200

    Added basic user creation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,16 @@
+const User = require("../models/userModel");
+
 const userLogin = (req, res) => {
     res.send("login route");
 };
 
-const userRegister = (req, res) => {
-    res.send("register route");
+const userRegister = async (req, res) => {
+    try {
+        const user = await User.create({ ...req.body });
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 };
 
 module.exports = { userLogin, userRegister };
