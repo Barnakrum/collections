288a3b2652caf9e63d8e5e6081b62b24f96da85c
Barnakrum <krzysiu03@tlen.pl>
2023-08-23 13:57:07 +0200

    Added basic collection post function

--- backend/controllers/collectionController.js
+++ backend/controllers/collectionController.js
@@ -1,5 +1,18 @@
+const Collection = require("../models/collectionModel");
+const User = require("../models/userModel");
+
+const jwt = require("jsonwebtoken");
+
 const postCollection = async (req, res) => {
-    res.send("x");
+    const { name, tags } = req.body;
+    const user = await User.findById(jwt.verify(req.cookies.session, process.env.TOKEN_KEY).user_id);
+
+    try {
+        const collection = await Collection.create({ name, tags, user: user._id });
+        res.status(201).send(collection);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 };
 
 module.exports = { postCollection };
