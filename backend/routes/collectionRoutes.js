5786e685363689d66b7f99ab771e388e58cd3ea2
Barnakrum <krzysiu03@tlen.pl>
2023-08-26 10:33:30 +0200

    Added multer middleware for uploading images

--- backend/routes/collectionRoutes.js
+++ backend/routes/collectionRoutes.js
@@ -2,7 +2,19 @@ const express = require("express");
 const auth = require("../middlewares/auth");
 const verifyMongoId = require("../middlewares/verifyMongoId");
 
-const { postCollection, getAllCollections, getCollection, patchCollection, deleteCollection } = require("../controllers/collectionController");
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "imagesForUpload/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.params.id + path.extname(file.originalname));
+    },
+});
+const upload = multer({ storage: storage });
+
+const { postCollection, getAllCollections, getCollection, patchCollection, deleteCollection, postCollectionImage } = require("../controllers/collectionController");
 
 const router = express.Router();
 
@@ -10,6 +22,7 @@ router.get("/", verifyMongoId("query"), getAllCollections);
 router.get("/:id", verifyMongoId(), getCollection);
 
 router.post("/", auth("collection"), postCollection);
+router.post("/:id/image", verifyMongoId(), auth("collection"), upload.single("image"), postCollectionImage);
 
 router.delete("/:id", verifyMongoId(), auth("collection"), deleteCollection);
 
