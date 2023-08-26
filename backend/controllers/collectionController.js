baec1ca84218d2cd6c5114d782b5661cd75bce80
Barnakrum <krzysiu03@tlen.pl>
2023-08-26 11:49:22 +0200

    Added function for posting and replacing images

--- backend/controllers/collectionController.js
+++ backend/controllers/collectionController.js
@@ -1,8 +1,20 @@
 const { default: mongoose } = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+//This is needed for cloudinary, otherwise it doesn't access the .env variables. I don't know why this happens because they are still avaiable for console.log
+const dotenv = require("dotenv");
+dotenv.config();
+//======================================================
+cloudinary.config({
+    cloud_name: "dggllsmjp",
+    api_key: "223753184872267",
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const Collection = require("../models/collectionModel");
 const User = require("../models/userModel");
-
-const jwt = require("jsonwebtoken");
 
 const postCollection = async (req, res) => {
     const { name, tags } = req.body;
@@ -11,6 +23,24 @@ const postCollection = async (req, res) => {
     try {
         const collection = await Collection.create({ name, tags, user: user._id });
         res.status(201).send(collection);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+const postCollectionImage = async (req, res) => {
+    try {
+        const collection = await Collection.findById({ _id: req.params.id });
+        const imagePath = "./imagesForUpload/" + collection._id + req.fileExtension;
+
+        if (!!collection.imageUrl) cloudinary.uploader.destroy(collection._id);
+        const result = await cloudinary.uploader.upload(imagePath, { public_id: collection._id });
+        collection.imageUrl = result.secure_url;
+
+        await fs.promises.unlink(imagePath);
+
+        await collection.save();
+        res.status(200).send(collection);
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -52,4 +82,4 @@ const getAllCollections = async (req, res) => {
     }
 };
 
-module.exports = { postCollection, deleteCollection, patchCollection, getAllCollections, getCollection };
+module.exports = { postCollection, deleteCollection, patchCollection, getAllCollections, getCollection, postCollectionImage };
