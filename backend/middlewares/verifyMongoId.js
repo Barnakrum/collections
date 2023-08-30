4d788975b65271b67d0d74ad0f1e1cb59cf6b44b
Barnakrum <krzysiu03@tlen.pl>
2023-08-30 18:02:43 +0200

    Abstracted functions from switch statement

--- backend/middlewares/verifyMongoId.js
+++ backend/middlewares/verifyMongoId.js
@@ -3,20 +3,24 @@ const mongoose = require("mongoose");
 const verifyMongoId = function (location) {
     switch (location) {
         default:
-            return function (req, res, next) {
-                if (!mongoose.isValidObjectId(req.params.id)) {
-                    return res.status(400).send("Please enter valid id");
-                }
-                next();
-            };
+            return checkUrlParams;
         case "query":
-            return function (req, res, next) {
-                if (!!req.query.user && !mongoose.isValidObjectId(req.query.user)) {
-                    return res.status(400).send("User id is invalid");
-                }
-                next();
-            };
+            return checkQueryParams;
     }
+};
+
+const checkQueryParams = function (req, res, next) {
+    if (!!req.query.user && !mongoose.isValidObjectId(req.query.user)) {
+        return res.status(400).send("User id is invalid");
+    }
+    next();
+};
+
+const checkUrlParams = function (req, res, next) {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send("Please enter valid id");
+    }
+    next();
 };
 
 module.exports = verifyMongoId;
