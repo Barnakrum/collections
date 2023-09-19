d24f77521bba48ee6c2d22199efba9a85f272dda
Barnakrum <krzysiu03@tlen.pl>
2023-09-19 14:40:13 +0200

    Added comments referencing resource id

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -7,12 +7,16 @@ const verifyMongoId = require("../middlewares/verifyMongoId");
 
 const { getItem, deleteItem, postItem, patchItem } = require("../controllers/itemController");
 
+//item id
 router.get("/:id", verifyMongoId(), getItem);
 
+//item id
 router.delete("/:id", verifyMongoId(), deleteItem);
 
+//collection id
 router.post("/:id", verifyMongoId(), auth("collection"), postItem);
 
+//item id
 router.patch("/:id", verifyMongoId(), patchItem);
 
 module.exports = router;
