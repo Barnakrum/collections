d6fa22d12f5275703ff4095ae8b3952915a92bf3
Barnakrum <krzysiu03@tlen.pl>
2023-09-21 14:14:28 +0200

    Added colors as extend of the theme from index.css vars

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--color-text)",
+        background: "var(--color-background)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
