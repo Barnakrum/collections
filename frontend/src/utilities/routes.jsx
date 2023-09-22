a649010f7d63971c46205613019f8aad42cd5b83
Barnakrum <krzysiu03@tlen.pl>
2023-09-22 20:41:45 +0200

    Added login and register routes Changed test-home to index

--- frontend/src/utilities/routes.jsx
+++ frontend/src/utilities/routes.jsx
@@ -1,10 +1,21 @@
 import Home from "../components/Home";
+import Login from "../components/auth/Login";
+import Register from "../components/auth/Register";
 
 const routes = [
-  {
-    path: "/test-home",
-    element: <Home />,
-  },
+    {
+        path: "/",
+        index: true,
+        element: <Home />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
 ];
 
 export default routes;
