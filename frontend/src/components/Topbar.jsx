80da680fb2fab89c7ff5a3bba8836533f66976ed
Barnakrum <krzysiu03@tlen.pl>
2023-09-23 13:09:28 +0200

    Added global css variable for topbar height

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,6 +1,6 @@
 const Topbar = (props) => {
     return (
-        <div className=" bg-text/10 flex min-h-[90px] w-full items-center justify-between p-4">
+        <div className=" bg-text/10 flex min-h-[var(--topbar-height)] w-full items-center justify-between p-4">
             {props.children}
         </div>
     );
