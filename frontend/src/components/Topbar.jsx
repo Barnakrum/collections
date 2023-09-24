8755d59d7e81c79202ecfc0d6fbb0aee85504409
Barnakrum <krzysiu03@tlen.pl>
2023-09-24 15:18:44 +0200

    Changed formatting

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,8 +1,4 @@
 const Topbar = (props) => {
-    return (
-        <div className=" bg-text/10 flex min-h-[var(--topbar-height)] w-full items-center justify-between p-4">
-            {props.children}
-        </div>
-    );
+    return <div className=" bg-text/10 flex min-h-[var(--topbar-height)] w-full items-center justify-between p-4">{props.children}</div>;
 };
 export default Topbar;
