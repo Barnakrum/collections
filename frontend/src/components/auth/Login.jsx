6185da344f07f7ad6668600b97c3d102f04df54b
Barnakrum <krzysiu03@tlen.pl>
2023-09-24 16:17:25 +0200

    Added button for login, and link to /register

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,20 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
     return (
         <>
-            <input
-                className=""
-                type="email"
-                placeholder="Email"
-                name=""
-                id=""
-            />
-            <input
-                className=""
-                type="password"
-                placeholder="Password"
-                name=""
-                id=""
-            />
+            <div className="flex flex-col max-w-lg gap-4 p-4 m-auto text-xl border rounded-lg bg-text/5 border-text/30">
+                <div className="text-3xl text-center text-primary">Login</div>
+                <div className="flex flex-col gap-4">
+                    <input className="" type="email" placeholder="Email" name="" id="email" />
+                    <div>
+                        <input className="w-full" type="password" placeholder="Password" name="" id="password" />
+                        <div>
+                            {/* TODO: forgot password needs implementation both frontend and backend */}
+                            <Link to={"/forgot-password"} className="pl-1 m-0 text-sm underline text-accent">
+                                Forgot password?
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex justify-between">
+                    <button className="w-1/2 rounded-l-lg form-button bg-primary">Login</button>
+                    <Link className="w-1/2 text-center rounded-r-lg form-button bg-primary/80" to={"/register"}>
+                        Register
+                    </Link>
+                </div>
+            </div>
         </>
     );
 };
