41070a6612b05d46a07d0bbf0185e83986842929
Barnakrum <krzysiu03@tlen.pl>
2023-09-25 15:59:20 +0200

    Changed development origin, allowed credentials

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 if (process.env.ENV === "DEVELOPMENT") {
-    app.use(cors({ origin: "*" }));
+    app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
     app.use(logger());
 } else {
-    app.use(cors({ origin: process.env.FRONTEND_ORIGIN }));
+    app.use(cors({ origin: process.env.FRONTEND_ORIGIN, credentials: true }));
 }
 
 app.get("/", (req, res) => {
