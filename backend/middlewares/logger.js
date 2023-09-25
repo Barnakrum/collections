52130b4be9d588fa2740d1c655ec3fbc3459645b
Barnakrum <krzysiu03@tlen.pl>
2023-09-25 18:57:56 +0200

    Now logs cookie "session"

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,6 +1,6 @@
 const logger = function () {
     return function (req, res, next) {
-        console.log(req.method + " | " + req.path + " | " + JSON.stringify(req.body));
+        console.log(req.method + " | " + req.path + " | " + JSON.stringify(req.body) + " | " + req.cookies.session);
 
         next();
     };
