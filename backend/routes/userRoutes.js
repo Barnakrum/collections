598219a67b30e87ef1d139060d8b28709b428b99
Barnakrum <krzysiu03@tlen.pl>
2023-09-26 16:28:55 +0200

    Added road for verification

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
-const { userLogin, userRegister } = require("../controllers/userController");
+const { userLogin, userRegister, userVerifyEmail } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/login", userLogin);
 
 router.post("/register", userRegister);
+
+router.get("/verify/:token/:email", userVerifyEmail);
 
 module.exports = router;
