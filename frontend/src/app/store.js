91e22b2c808babba91bfe7b21d3f6269ff61705c
Barnakrum <krzysiu03@tlen.pl>
2023-09-27 17:34:59 +0200

    Added session reducer

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { backendApi } from "../services/backend";
+import sessionReducer from "./slices/session";
 
 export const store = configureStore({
     reducer: {
         [backendApi.reducerPath]: backendApi.reducer,
+        session: sessionReducer,
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(backendApi.middleware),
