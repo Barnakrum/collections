959be8cf170d1653fca72cec3a757cb9a9305479
Barnakrum <krzysiu03@tlen.pl>
2023-09-28 15:35:26 +0200

    Added localhost as allowed origin

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 if (process.env.ENV === "DEVELOPMENT") {
-    app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+    app.use(cors({ origin: ["http://127.0.0.1:5173", "http://localhost:5173"], credentials: true }));
     app.use(logger());
 } else {
     app.use(cors({ origin: process.env.FRONTEND_ORIGIN, credentials: true }));
