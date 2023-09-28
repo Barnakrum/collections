957f30103f2ffd928156bd5f78452e7f4245f41f
Barnakrum <krzysiu03@tlen.pl>
2023-09-28 15:37:18 +0200

    Checks if user islogged in and refreshes token if token is invalid logs out

--- frontend/src/components/auth/DisplayUser.jsx
+++ frontend/src/components/auth/DisplayUser.jsx
@@ -1,12 +1,30 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-
-useEffect(() => {}, []);
+import { useSelector, useDispatch } from "react-redux";
+import { useGetRefreshTokenQuery, useLazyLogoutQuery } from "../../services/backend";
+import { login, logout } from "../../App/slices/session";
 
 const DisplayUser = () => {
     const { username, isLoggedIn } = useSelector((state) => state.session);
+    const { data, error, isFetching, isLoading, isSuccess, isError } = useGetRefreshTokenQuery();
+    const [triggerLogout] = useLazyLogoutQuery();
+    const dispatch = useDispatch();
 
-    return <div className={isLoggedIn ? "" : "hidden"}>Hello, {username}</div>;
+    useEffect(() => {
+        if (!isLoading) {
+            if (!isError) {
+                dispatch(login({ id: data.id, username: data.username, isLoggedIn: true }));
+            } else {
+                triggerLogout();
+                dispatch(logout());
+            }
+        }
+    }, [isLoading]);
+
+    return (
+        <div>
+            <div className={isLoggedIn ? "" : "hidden"}>Hello, {username}</div>
+        </div>
+    );
 };
 
 export default DisplayUser;
