3491512f1d30bee2937b55e2090a7687dc6e9745
Barnakrum <krzysiu03@tlen.pl>
2023-09-28 15:36:11 +0200

    Added routes for refresh token and logging out

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
-const { userLogin, userRegister, userVerifyEmail } = require("../controllers/userController");
+const { userLogin, userRegister, userVerifyEmail, getRefreshToken, userLogout } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/login", userLogin);
 
+router.get("/logout", userLogout);
+
 router.post("/register", userRegister);
 
 router.get("/verify/:token/:email", userVerifyEmail);
+
+router.get("/get-refresh-token", getRefreshToken);
 
 module.exports = router;
