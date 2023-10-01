f7b521565fe9e0cd09b1a774cb739c13b77614af
Barnakrum <krzysiu03@tlen.pl>
2023-10-01 17:26:35 +0200

    Cleaned up links They are now mapped from array

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -6,7 +6,32 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLazyLogoutQuery } from "../services/backend";
 
 const Menu = () => {
-    const menuLinks = [{ to: "/", text: "Home" }];
+    const isLoggedIn = useSelector((state) => state.session.isLoggedIn);
+
+    const menuLinks = [
+        { to: "/", text: "Home", callback: () => {} },
+        isLoggedIn
+            ? {
+                  to: "/",
+                  text: "Logout",
+                  callback: () => {
+                      dispatch(logout());
+                      triggerLogout();
+                  },
+              }
+            : {
+                  to: "/login",
+                  text: "Login",
+                  callback: () => {},
+              },
+        isLoggedIn
+            ? {
+                  to: "/collection/post",
+                  text: "Add collection",
+                  callback: () => {},
+              }
+            : "",
+    ];
 
     const [triggerLogout] = useLazyLogoutQuery();
 
@@ -15,7 +40,6 @@ const Menu = () => {
     const [menuContent, setMenuContent] = useState(document.getElementById("menu-content"));
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const isLoggedIn = useSelector((state) => state.session.isLoggedIn);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -44,10 +68,13 @@ const Menu = () => {
                 }>
                 <div className="flex flex-col w-full h-full text-2xl text-right bg-text/20 md:flex-row md:items-center md:bg-transparent md:text-lg">
                     {menuLinks.map((link, index) => {
-                        return (
+                        return link === "" ? (
+                            <></>
+                        ) : (
                             <Link
                                 className="p-2 hover:bg-text/50 md:hover:bg-transparent md:hover:text-primary md:h-full md:flex md:items-center md:px-6 "
                                 onClick={() => {
+                                    link.callback();
                                     toggleMenu();
                                 }}
                                 key={index}
@@ -60,23 +87,11 @@ const Menu = () => {
                     <div className="w-full p-2 md:px-6 md:h-full md:flex md:items-center md:hover:bg-transparent hover:bg-text/50">
                         <ThemeSelector />
                     </div>
-                    <Link
-                        className={(isLoggedIn ? "hidden" : "md:flex") + " p-2 hover:bg-text/50 md:hover:bg-transparent md:hover:text-primary md:h-full  md:items-center md:px-6"}
-                        onClick={() => {
-                            toggleMenu();
-                        }}
-                        to="/login">
-                        Login
-                    </Link>
                     <button
-                        className={(isLoggedIn ? "md:flex" : "hidden") + " p-2 text-right hover:bg-text/50 md:hover:bg-transparent md:hover:text-primary md:h-full md:items-center md:px-6 "}
                         onClick={() => {
-                            dispatch(logout());
-                            triggerLogout();
-                            navigate("/");
-                            toggleMenu();
+                            console.log(menuLinks);
                         }}>
-                        Logout
+                        conosle
                     </button>
                 </div>
             </div>
