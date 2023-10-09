6a69fd70f477277831ca1f32c50669a09a3a2c8c
Barnakrum <krzysiu03@tlen.pl>
2023-10-09 13:00:33 +0200

    Initial commit

---
+++ frontend/src/components/item/PostItem.jsx
@@ -0,0 +1,103 @@
+import { useParams } from "react-router-dom";
+import { useGetCollectionQuery, usePostItemMutation } from "../../services/backend";
+import Spinner from "../utility/Spinner";
+import { useEffect, useState } from "react";
+
+const PostItem = () => {
+    const { id } = useParams();
+    const { data, isLoading, isSuccess, isError, error } = useGetCollectionQuery(id);
+
+    const [postItem, postItemResult] = usePostItemMutation();
+
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const [stringFields, setStringFields] = useState([]);
+    const [booleanFields, setBooleanFields] = useState([]);
+    const [numberFields, setNumberFields] = useState([]);
+    const [dateFields, setDateFields] = useState([]);
+    const [colorFields, setColorFields] = useState([]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const item = { name, description, stringFields, booleanFields, numberFields, dateFields, colorFields };
+        const response = await postItem({ id, payload: item });
+    };
+
+    if (isLoading) {
+        return <Spinner />;
+    } else if (isError) {
+        return <div className="text-error">{error.message}</div>;
+    }
+    // TODO: Navigate to display page if posting is successful or display error
+
+    return (
+        <form
+            onSubmit={(event) => {
+                handleSubmit(event);
+            }}
+            className="flex mt-5 flex-col max-w-[80%] md:max-w-lg gap-4 p-4 m-auto text-xl border rounded-lg  bg-text/5 border-text/30">
+            <h1 className="text-3xl text-center ">
+                New item for <span className="text-primary">{data.name}</span>
+            </h1>
+
+            <label>Name</label>
+            <input
+                onChange={(event) => {
+                    setName(event.target.value);
+                }}
+                required={true}
+                type="text"
+            />
+            <label>Decription</label>
+            <textarea
+                onChange={(event) => {
+                    setDescription(event.target.value);
+                }}
+                required={true}
+                cols={30}
+            />
+            <FieldNames data={data.stringFieldsNames} fields={stringFields} setFields={setStringFields} inputType="text" />
+            <FieldNames data={data.booleanFieldsNames} fields={booleanFields} setFields={setBooleanFields} inputType="checkbox" />
+            <FieldNames data={data.colorFieldsNames} fields={colorFields} setFields={setColorFields} defaultValue="#ff00ff" inputType="color" />
+            <FieldNames data={data.numberFieldsNames} fields={numberFields} setFields={setNumberFields} inputType="number" />
+            <FieldNames data={data.dateFieldsNames} fields={dateFields} setFields={setDateFields} inputType="datetime-local" />
+            <button className="rounded-md form-button bg-primary">Submit</button>
+        </form>
+    );
+};
+
+export default PostItem;
+
+function FieldNames({ data, inputType, setFields, fields, defaultValue }) {
+    return (
+        <div>
+            {data.map((field, index) => {
+                return (
+                    <div key={index} className="flex flex-col gap-1">
+                        <label>{field}</label>
+                        <input
+                            required={inputType === "checkbox" ? false : true}
+                            type={inputType}
+                            onChange={(event) => {
+                                setFields(
+                                    data.map((f, i) => {
+                                        if (i === index) {
+                                            if (inputType === "checkbox") {
+                                                return event.target.checked;
+                                            }
+                                            return event.target.value;
+                                        } else {
+                                            return fields[i];
+                                        }
+                                    })
+                                );
+                            }}
+                            defaultValue={defaultValue}
+                        />
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
