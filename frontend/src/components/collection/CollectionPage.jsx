28142a3863156492c5381f399e0afc8a34a310ba
Barnakrum <krzysiu03@tlen.pl>
2023-10-09 19:43:51 +0200

    Now uses global state to conditonally render buttons that need authorization

--- frontend/src/components/collection/CollectionPage.jsx
+++ frontend/src/components/collection/CollectionPage.jsx
@@ -3,10 +3,14 @@ import { useDeleteCollectionImageMutation, useGetCollectionQuery, useGetUserQuer
 import Spinner from "../utility/Spinner";
 import DisplayFieldsNames from "./DisplayFieldsNames";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const CollectionPage = () => {
     const { id } = useParams();
     const navigate = useNavigate();
+
+    const userId = useSelector((state) => state.session.id);
+    const isAdmin = useSelector((state) => state.session.isAdmin);
 
     const { data, error, isError, isUninitialized, isLoading, isSuccess } = useGetCollectionQuery(id);
     const { data: user, isSuccess: userIsSuccess } = useGetUserQuery(isSuccess ? data.user : { skip: true });
@@ -35,16 +39,22 @@ const CollectionPage = () => {
                     </div>
                     <div className="relative md:max-w-3xl ">
                         <img src={data.imageUrl} className="md:m-auto md:rounded-3xl " alt="" />
-                        <Link to={"/collection/postImage/" + data._id} className="absolute flex justify-center p-2 border-2 rounded-3xl bg-transparent/80 right-2 top-2 md:right-4 md:top-4 hover:bg-transparent border-secondary text-secondary">
-                            <span className="material-symbols-outlined">edit</span>
-                        </Link>
-                        <button
-                            onClick={() => {
-                                handleDelete();
-                            }}
-                            className="absolute flex justify-center p-2 border-2 rounded-3xl bg-transparent/80 right-2 bottom-2 md:right-4 md:bottom-4 hover:bg-transparent border-error text-error">
-                            <span className="material-symbols-outlined">delete_forever</span>
-                        </button>
+                        {userId === data.user || isAdmin ? (
+                            <>
+                                <Link to={"/collection/postImage/" + data._id} className="absolute flex justify-center p-2 border-2 rounded-3xl bg-transparent/80 right-2 top-2 md:right-4 md:top-4 hover:bg-transparent border-secondary text-secondary">
+                                    <span className="material-symbols-outlined">edit</span>
+                                </Link>
+                                <button
+                                    onClick={() => {
+                                        handleDelete();
+                                    }}
+                                    className="absolute flex justify-center p-2 border-2 rounded-3xl bg-transparent/80 right-2 bottom-2 md:right-4 md:bottom-4 hover:bg-transparent border-error text-error">
+                                    <span className="material-symbols-outlined">delete_forever</span>
+                                </button>
+                            </>
+                        ) : (
+                            ""
+                        )}
                     </div>
                     <div className="flex flex-col p-3 m-auto bg-text/10 rounded-b-3xl md:max-w-xl">
                         <div className="flex gap-2">
@@ -72,13 +82,19 @@ const CollectionPage = () => {
                 <DisplayFieldsNames fields={data.numberFieldsNames} name={"number fields"} />
                 <DisplayFieldsNames fields={data.dateFieldsNames} name={"date fields"} />
                 <div className="flex flex-row max-w-[8rem]">
-                    <Link className="flex items-center justify-center bg-transparent border-2 rounded-l-lg hover:text-text hover:bg-accent border-accent text-accent grow bg-primary" to={"/item/post/" + id}>
-                        <span className="material-symbols-outlined">add_ad</span>
-                    </Link>
-                    {/* TODO: add deleting modal with confirm */}
-                    <button type="button" className="flex items-center justify-center bg-transparent border-2 rounded-r-lg hover:bg-error/50 border-error text-error grow bg-primary">
-                        <span className="material-symbols-outlined">folder_delete</span>
-                    </button>
+                    {userId === data.user || isAdmin ? (
+                        <>
+                            <Link className="flex items-center justify-center bg-transparent border-2 rounded-l-lg hover:text-text hover:bg-accent border-accent text-accent grow bg-primary" to={"/item/post/" + id}>
+                                <span className="material-symbols-outlined">add_ad</span>
+                            </Link>
+                            {/* TODO: add deleting modal with confirm */}
+                            <button type="button" className="flex items-center justify-center bg-transparent border-2 rounded-r-lg hover:bg-error/50 border-error text-error grow bg-primary">
+                                <span className="material-symbols-outlined">folder_delete</span>
+                            </button>{" "}
+                        </>
+                    ) : (
+                        ""
+                    )}
                 </div>
             </div>
         );
