7476631d798e9a94a7f71fb6764eeda06b45686b
Barnakrum <krzysiu03@tlen.pl>
2023-10-18 15:25:04 +0200

    Returns spinner if didnt finish loading

--- frontend/src/components/user/UsernameLink.jsx
+++ frontend/src/components/user/UsernameLink.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { useGetUserQuery } from "../../services/backend";
+import Spinner from "../utility/Spinner";
 
 const UsernameLink = ({ id }) => {
     const { data, isLoading } = useGetUserQuery(id);
 
+    if (isLoading) {
+        return <Spinner />;
+    }
     return (
         <Link className="underline" to={"/user/" + id}>
             {data.username}
