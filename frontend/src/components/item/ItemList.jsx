15c5d392abf3d3a2a1b9f76e95b651f12d5cb298
Barnakrum <krzysiu03@tlen.pl>
2023-10-26 15:14:50 +0200

    Added buttons on hover for deleting and modyfing

--- frontend/src/components/item/ItemList.jsx
+++ frontend/src/components/item/ItemList.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import { useGetAllItemsQuery } from "../../services/backend";
 import Spinner from "../utility/Spinner";
+import { useSelector } from "react-redux";
 
 const ItemList = ({ id }) => {
     const { data, isLoading, isError, error } = useGetAllItemsQuery({ collectionId: id });
+
+    const userId = useSelector((state) => state.session.id);
+    const isAdmin = useSelector((state) => state.session.isAdmin);
 
     if (isLoading) {
         return <Spinner />;
@@ -11,19 +15,27 @@ const ItemList = ({ id }) => {
     if (isError) {
         return <div>{error.data.message}</div>;
     }
-    console.log(data);
     return (
         <div>
             {data.map((item, index) => (
-                <ItemCard key={index} item={item} />
+                <ItemCard key={index} item={item} canModify={isAdmin || item.user === userId} />
             ))}
         </div>
     );
 };
 
-const ItemCard = ({ item }) => {
+const ItemCard = ({ item, canModify }) => {
     return (
-        <div className="flex flex-col gap-2 p-2 border-2 border-primary/30">
+        <div className="relative flex flex-col gap-2 p-2 border-2 border-primary/30 group">
+            <div className="absolute flex invisible top-2 right-2 group-hover:visible">
+                {/* {TODO: add delete and edit functionality} */}
+                <button type="button">
+                    <span className="text-error hover:animate-pulse material-symbols-outlined">delete_forever</span>
+                </button>
+                <button type="button">
+                    <span className="text-accent hover:animate-pulse material-symbols-outlined">edit</span>
+                </button>
+            </div>
             <Link to={"/item/" + item._id}>
                 <p className="text-2xl text-center underline">{item.name}</p>
             </Link>
